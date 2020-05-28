be1d59ee0a707b37ed3c56c5441466a34f815161
Roddy <rodtai2019@gmail.com>
2020-05-28 13:51:39 -0400

    Add home page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,14 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomTVShowQuote() {
+  const quotes =
+      ["I'M PICKLE RICK!", 'Bears Beets Battlestar Galactica.', 'Can I offer you an egg in this trying time?'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
 }
