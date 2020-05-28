35dac1252bb3dd87760893c384eecb1f84c54a9e
Roddy <rodtai2019@gmail.com>
2020-05-28 18:29:05 -0400

    Add JQuery to load navbar into multiple files

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-
+$(function(){
+    $("nav").load("nav.html");
+});
