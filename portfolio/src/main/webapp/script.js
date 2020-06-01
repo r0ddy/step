d4089c7b44a815ea9fbfa98ba67a0584e910f248
Roddy <rodtai2019@gmail.com>
2020-06-01 12:37:15 -0400

    Add templating to make multiple blog entries

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -70,3 +70,51 @@ function loadProjects(){
         document.body.querySelector('.projects').appendChild(clone);
     });
 }
+
+function loadBlogEntries(){
+    let blogEntries = [
+        {
+            image: {
+                src: "/images/google.jpg",
+                alt: "Google logo."
+            },
+            text: {
+                title: "Google Internship: Week 2",
+                date: "May 26, 2020",
+                body: 
+                "The reason I start my blog with a post about my second week \
+                at Google is because not much happened during my short week 1. \
+                During my first week, I was just setting up my machine and \
+                getting to know my hosts and pod. It was during my second week \
+                that I learned more."
+            },
+            link: ""
+        },
+        {
+            image: {
+                src: "/images/google.jpg",
+                alt: "Google logo."
+            },
+            text: {
+                title: "Google Internship: Week 3",
+                date: "June 1, 2020",
+                body: 
+                "Week 3 has just begun. I am suppose to wrap up my week 2 \
+                project (this website), but I think I might have to spend a \
+                more time trying to complete my blog."
+            },
+            link: ""
+        }
+    ];
+    let blogEntryTemplate = document.querySelector('#blogEntryTemplate')
+    blogEntries.forEach((blogEntry) => {
+        blogEntryTemplate.content.querySelector('a.blogLink').href = blogEntry.link;
+        blogEntryTemplate.content.querySelector('img.blogThumbnailImage').src = blogEntry.image.src;
+        blogEntryTemplate.content.querySelector('img.blogThumbnailImage').alt = blogEntry.image.alt;
+        blogEntryTemplate.content.querySelector('h4.blogSummaryTitle').textContent = blogEntry.text.title;
+        blogEntryTemplate.content.querySelector('p.blogSummaryDate').textContent = blogEntry.text.date;
+        blogEntryTemplate.content.querySelector('p.blogSummaryText').textContent = blogEntry.text.body;
+        let clone = document.importNode(blogEntryTemplate.content, /*deep_copy=*/ true);
+        document.body.querySelector('.textBlurb').appendChild(clone);
+    });
+}   
